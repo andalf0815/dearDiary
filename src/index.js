e61d1d78f4adefaa2dfab6eb75d704705b6fe1ad
Andreas Gruber <andreas-gruber@outlook.at>
2022-08-03 19:36:52 +0200

    finished password validation when submitting form

--- src/index.js
+++ src/index.js
@@ -5,26 +5,43 @@ const $register = document.querySelector("#btn_register");
 const $submit = document.querySelector('[type="submit"]');
 const $pswList = document.querySelectorAll('[type="password"]');
 
-const $validation = document.querySelector("#span_validation");
+const $validation = document.querySelector("#p_validation");
+
+/**********/
+/* EVENTS */
+/**********/
+
+// Check the password validation before the submit action will be triggered
+// First step = Stop the form on submitting
+// Second step = Check if the password matches -> If yes, then submit the form
+
+$form.addEventListener("submit", (event) => {
+  if($form.dataset.login === undefined){
+    event.preventDefault();
+    if ($validation.dataset.matching === "true") {
+      $form.submit();
+    }
+  }
+});
 
 // Set an eventListener to toggle between the login view
 // and the register view
 
 $register.addEventListener("click", () => {
-  if ($form.dataset.login === undefined) {
+  if ($form.dataset.login !== undefined) {
     // Click on Sign Up -> switch to register view from sign in view and set action to ?register
 
     $form.setAttribute("action", "/?register");
     $submit.value = "Sign Up";
     $register.textContent = "Log In";
-    $validation.hidden = true;
+    $validation.hidden = false;
   } else {
     // Click on Log In -> switch to login view register view and set action to ?login
 
     $form.setAttribute("action", "/?login");
     $submit.value = "Log In";
     $register.textContent = "Sign Up";
-    $validation.hidden = false;
+    $validation.hidden = true;
   }
 
   // Toogle the second password input field and the data-login attribute in frm_login
@@ -34,7 +51,7 @@ $register.addEventListener("click", () => {
   $pswList[1].toggleAttribute("required");
 });
 
-// Repeated password validation -> Check if the repeated password is the same as the first entered
+// EventListener for validating the repeated password -> Check if the repeated password is the same as the first entered
 
 for (let $element of $pswList) {
   $element.addEventListener("keyup", () => {
